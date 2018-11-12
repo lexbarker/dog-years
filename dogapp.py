e30adc68a62ea72088bc37cb882633e2a001976a
Lex Barker <lex1903@gmail.com>
2018-11-12 21:06:40 +0000

    added the dog age call code to sewt URL to environment

--- dogapp.py
+++ dogapp.py
@@ -1,19 +1,26 @@
+import os
 from flask import Flask
 from flask import render_template
 from flask import request
 app = Flask(__name__)
 
+if "DOG_ENV" in os.environ:
+    DOGTAG = "https://dog-years.herokuapp.com"
+else:
+    DOGTAG = "http://localhost:5000"
+
+
 @app.route('/', methods=['GET'])
 def home():
     if request.method == "GET":
-        return render_template('homepage.html')
+        return render_template('homepage.html', addy = DOGTAG)
 
 @app.route('/calc', methods=['POST'])
 def calc_dog_years():
     dage = request.form['years']
     dog_age = int(dage) * 7
 
-    return render_template('dogyears.html', age = dog_age)
+    return render_template('dogyears.html', addy = DOGTAG, age = dog_age)
 if __name__ == '__main__':
 
    app.run()
