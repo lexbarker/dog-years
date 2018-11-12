5fb7ef3bd59ef7866b2689f08bd27aa06b8ad6e0
Lex Barker <lex1903@gmail.com>
2018-11-12 20:49:12 +0000

    added the doge age call code

--- dogapp.py
+++ dogapp.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask import render_template
+from flask import request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return 'Hello World'
+@app.route('/', methods=['GET'])
+def home():
+    if request.method == "GET":
+        return render_template('homepage.html')
 
+@app.route('/calc', methods=['POST'])
+def calc_dog_years():
+    dage = request.form['years']
+    dog_age = int(dage) * 7
+
+    return render_template('dogyears.html', age = dog_age)
 if __name__ == '__main__':
+
    app.run()
